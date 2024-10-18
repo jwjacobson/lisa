21b81328ad94451fa485aaf9e64fadd94625b993
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2024-10-18 13:32:22 -0400

    First commit

---
+++ hello.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from lisa!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ lisa/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-0t7!ya*idt9jz(dl4&p(zlw6gglswc#-%_kebc00b^0)(*ffqa"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
